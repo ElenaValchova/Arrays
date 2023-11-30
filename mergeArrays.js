2b3892afaa34b32d24e1e8931968bf755decb80c
Elena Valchova <elenavalcheva99@abv.bg>
2023-11-30 10:52:50 +0200

    Resolved the thired problem

---
+++ mergeArrays.js
@@ -0,0 +1,22 @@
+function mergeArrays(arr1, arr2) {
+    let mergedArray = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        let num1 = arr1[i];
+        let num2 = arr2[i];
+
+        if (i % 2 == 0) {
+            let sum = Number(num1) + Number(num2);
+            mergedArray.push(sum);
+        } else {
+            let concatenation = num1 + num2;
+            mergedArray.push(concatenation);
+        }
+
+
+    }
+    console.log(mergedArray.join(" - "));
+}
+
+// mergeArrays(['5', '15', '23', '56', '35'], ['17', '22', '87', '36', '11']);
+mergeArrays(['13', '12312', '5', '77', '4'], ['22', '333', '5', '122', '44']);
