93ce379ee9f06a40e549b865538e3863b3bf2af1
Elena Valchova <elenavalcheva99@abv.bg>
2023-12-07 09:53:23 +0200

    Resolved the second problem from function exercise

---
+++ mathPower.js
@@ -0,0 +1,13 @@
+function mathPower(number, power) {
+    let result = 1;
+    for (let i = 0; i < power; i++) {
+        result = multiply(result, number);
+    }
+    console.log(result);
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+mathPower(3, 4);
