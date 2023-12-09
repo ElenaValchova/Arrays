0a70f6c0b1d7666045d0fc9d9519f8ec76085fdc
Elena Valchova <elenavalcheva99@abv.bg>
2023-12-09 10:47:25 +0200

    First problemresolved from function exercise

--- check.js
+++ check.js
@@ -1,6 +1,4 @@
 function check(numOne, numTwo, numTree, numFour) {
-    let res = numOne * numTwo * numTree * numFour
-    console.log(res);
     let arr = [];
     arr.push(numOne, numTwo, numTree, numFour);
 
@@ -9,23 +7,17 @@ function check(numOne, numTwo, numTree, numFour) {
     for (let i = 0; i < arr.length; i++){
         let curDigit = arr[i];
 
-        if (curDigit < 0){
+        if (curDigit <= 0){
             countNegative++
         }
     }
 
 
-    if (countNegative == 1 || countNegative % 3 == 0 || countNegative % 2 === 0) {
+    if (countNegative == 1 || countNegative % 3 == 0) {
         console.log("Negative");
     } else {
         console.log("Positive");
     }
 }
 
-check(-5,
-
-    -12,
-    
-    -15,  
-
-    - 12);
+check(-5, -12, -15,);
---
+++ smallest.js
@@ -0,0 +1,9 @@
+function smallest(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));
+}
+
+smallest(2,
+
+    2,
+    
+    2);
